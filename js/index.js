bec69144eb0d16ae23c5d811fafdbc7fe4894e85
suneel kumar yarramuthala <yarramuthalasunil@gmail.com>
2022-10-03 16:57:53 +0530

    addes the product page that is intermediate between cart

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function increaseCount(a, b) {
+  var input = b.previousElementSibling;
+  var value = parseInt(input.value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  input.value = value;
+}
+
+function decreaseCount(a, b) {
+  var input = b.nextElementSibling;
+  var value = parseInt(input.value, 10);
+  if (value > 1) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    input.value = value;
+  }
+}
+function GoToContact(){
+  location.replace("contact.html")
+}
+function productpage(){
+  location.replace("product.html")
+}
